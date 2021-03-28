ba1cb82c82846344212297ad00956317de2d733d
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-03-28 19:20:11 +0800

    Update usb_printer.lua

--- luasrc/controller/usb_printer.lua
+++ luasrc/controller/usb_printer.lua
@@ -21,9 +21,9 @@ function index()
 		return
 	end
 	
-  entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
+  entry({"admin", "services"}, firstchild(), "Services", 44).dependent = false
 	
 	local page
 
-	page = entry({"admin", "nas", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 50)
+	page = entry({"admin", "services", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 50)
 end
